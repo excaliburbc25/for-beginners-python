6608f69341a400b6c4a94331c410bfd3c9e8fc75
excaliburbc25 <excaliburbc25@gmail.com>
2026-05-27 13:46:15 +0800

    Delete Chapter 7 directory

--- Chapter 7/greeter.py
+++
@@ -1,8 +0,0 @@
-name = input("\nWhat is your name: ")
-print(f"Hello to you {name.title()}\n")
-
-prompt = "If you tell us who you are, we can personalize the messages you see."
-prompt += "\nWhat is your first name? "
-
-name = input(prompt)
-print(f"\nhello , {name}!")
--- Chapter 7/parrot.py
+++
@@ -1,2 +0,0 @@
-message = input("Tell me something, and I'll repeart it back to you: ")
-print(message)
