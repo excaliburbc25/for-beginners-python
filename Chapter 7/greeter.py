409b231650f74aef85ac5e8b2ff367cbe2c7518f
excaliburbc25 <excaliburbc25@gmail.com>
2026-05-27 13:45:24 +0800

    Add files via upload

---
+++ Chapter 7/greeter.py
@@ -0,0 +1,8 @@
+name = input("\nWhat is your name: ")
+print(f"Hello to you {name.title()}\n")
+
+prompt = "If you tell us who you are, we can personalize the messages you see."
+prompt += "\nWhat is your first name? "
+
+name = input(prompt)
+print(f"\nhello , {name}!")
---
+++ Chapter 7/parrot.py
@@ -0,0 +1,2 @@
+message = input("Tell me something, and I'll repeart it back to you: ")
+print(message)
